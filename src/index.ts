49cf6d1dde0f494dc8eaf2487fb18a9d6af566b9
daymadev89 <daymadev89@gmail.com>
2025-08-07 20:02:53 +0800

    chore: bump version to 1.5.1

--- src/index.ts
+++ src/index.ts
@@ -79,7 +79,7 @@ handleSmartRouting();
 program
   .name(isAiunlock ? 'aiunlock' : 'ailock')
   .description('AI-Proof File Guard - Protect sensitive files from accidental AI modifications')
-  .version('1.5.0');
+  .version('1.5.1');
 
 // Add commands
 program.addCommand(initCommand);
